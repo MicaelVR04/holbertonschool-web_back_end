9df72696bec9453ea84c433d6fc10f01414d9ad7
Micael Velez <mvrod2314@gmail.com>
2026-04-06 00:31:01 +0000

    mul

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for creating multiplier functions."""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by multiplier."""
+    def multiply(value: float) -> float:
+        """Multiply value by the captured multiplier."""
+        return value * multiplier
+    return multiply
