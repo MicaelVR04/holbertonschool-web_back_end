db258a32e10886c806339f837ecf9dee55cc9351
Micael Velez <mvrod2314@gmail.com>
2026-04-06 14:45:14 +0000

    asyncgen

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async generator module."""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yield 10 random floats between 0 and 10, one per second."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
